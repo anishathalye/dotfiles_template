1db68ff9c6b02eb19077918e4a4adb33c404aced
Kraig McKernan <kraigmckernan@gmail.com>
2016-10-07 21:25:16 -0600

    Allow a dir parameter for windows that specifies where you want to work

--- bash/tmuxifier.sh
+++ bash/tmuxifier.sh
@@ -1,10 +1,18 @@
 TMUXIFIER_BIN="$DOTFILES/tmuxifier/git/bin"
 TMUX_VERSION=$(tmux -V 2>/dev/null | cut -f2 -d" ")
+
+create_windows() {
+  export PROJ="$1"
+  tmuxifier load-session kraig
+}
+
 if [[ $? == 0 && "$TMUX_VERSION" > "1.6" && -f "$TMUXIFIER_BIN/tmuxifier" ]]; then
   export PATH="$TMUXIFIER_BIN:$PATH"
   export TMUXIFIER_LAYOUT_PATH="$DOTFILES/tmuxifier/layout"
   eval "$(tmuxifier init -)"
-  alias windows="tmuxifier load-session kraig"
+  # Usage `windows ~/root`
+  # Uses given dir for the window root
+  alias windows=create_windows
   alias kill-windows="tmux kill-window -t kraig"
 else
   alias windows="echo 'tmux must be installed and >version 1.6'"
--- tmuxifier/layout/code.window.sh
+++ tmuxifier/layout/code.window.sh
@@ -1,6 +1,6 @@
 # Set window root path. Default is `$session_root`.
 # Must be called before `new_window`.
-#window_root "~/Projects/code"
+window_root $PROJ
 
 # Create new window. If no argument is given, window name will be based on
 # layout file name.
