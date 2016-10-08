5bacc23ad90322aa727fc7fad60763edac156f08
Kraig McKernan <kraigmckernan@gmail.com>
2016-10-07 20:41:58 -0600

    Use checkwinsize in bash for tmux

--- bash/general.sh
+++ bash/general.sh
@@ -5,3 +5,6 @@ export EDITOR="vim"
 BASE16_SHELL="$DOTFILES/base16/base16-shell/"
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
 base16_atelier-forest
+
+# Check the win size since tmux will change the size often
+shopt -s checkwinsize
