78eb54fd0e2eab1cba00b8dab6c6844027e54d47
Ethan Miller <ethanzanemiller@gmail.com>
2019-05-30 09:50:49 -0400

    Add bootstrap for work dev

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -2,3 +2,7 @@ path_prepend "$HOME/.dotfiles/bin"
 
 # For Hub (http://hub.github.com)
 eval "#(hub alias -s)"
+
+# Setup docker machine env defaults
+# Note: Only works if docker host `default` is running
+eval $(docker-machine env default)
