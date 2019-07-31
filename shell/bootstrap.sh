60d00d2589115a41ebbe94fc2d5322dfc42c6f0c
Ethan Miller <ethanzanemiller@gmail.com>
2019-07-31 16:36:28 -0400

    Fix typo

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -4,7 +4,7 @@ set -o emacs
 path_prepend "$HOME/.dotfiles/bin"
 
 # For Hub (http://hub.github.com)
-eval "#(hub alias -s)"
+eval "$(hub alias -s)"
 
 # Setup docker machine env defaults
 # Note: Only works if docker host `default` is running
