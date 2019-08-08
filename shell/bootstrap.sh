069cd04ec4239e54a735bd99e56e4eba7bf746be
Ethan Miller <ethanzanemiller@gmail.com>
2019-08-08 09:49:25 -0400

    Add initialize for pyenv

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -6,6 +6,9 @@ path_prepend "$HOME/.dotfiles/bin"
 # For pyenv: https://github.com/pyenv/pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 path_prepend "$PYENV_ROOT/bin"
+if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init -)"
+fi
 
 # For Hub (http://hub.github.com)
 eval "$(hub alias -s)"
