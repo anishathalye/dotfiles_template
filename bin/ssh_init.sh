ea0e40783c7efbcae541a00cb75175e31118e013
lbesnard <laurent.besnard@utas.edu.au>
2019-11-18 16:22:39 +1100

    wip

---
+++ bin/ssh_init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# script to setup ssh on someone's computer with limited linux knowledged
+# curl -L https://raw.githubusercontent.com/lbesnard/dotfiles/bin/ssh_init.sh | bash
+
+set -e
+sudo apt-get -y install openssh-server
+sudo systemctl enable ssh
+sudo systemctl start ssh
+
+mkdir -p $HOME/.ssh -m 700;
+curl -L https://github.com/lbesnard.keys -o ~/.ssh/id_rsa.pub;
+chmod 644 $HOME/.ssh/id_rsa.pub;
+echo 'ssh key for lbesnard initialised'
