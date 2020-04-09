28ead249db487ab00d213cc8c0916ab761e2350e
Ethan Miller <ethanzanemiller@gmail.com>
2020-04-09 15:39:09 -0400

    Add new mapping to return to evil-normal-state

--- spacemacs-private/ezfm-config/keybindings.el
+++ spacemacs-private/ezfm-config/keybindings.el
@@ -3,3 +3,6 @@
           (lambda ()
             (local-set-key "\C-cy" 'notespace/compute-note-at-this-line)
             (local-set-key "\C-cu" 'notespace/compute-this-notespace)))
+
+;; Trying this out - might work better w/ minivan keyboard
+(define-key evil-insert-state-map (kbd "C-;") 'evil-normal-state)
