66463173ebd705ecae9c3ce7fb25a6725816e8f7
Kraig McKernan <kraigmckernan@gmail.com>
2020-04-11 12:30:09 -0600

    Fixes for new fedora install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,11 @@
 if [[ -f "/etc/fedora-release" ]];
 then
-  sudo dnf install -y tilix NetworkManager-tui the_silver_searcher neovim tmux rofi arandr pavucontrol java-1.8.0-openjdk-devel feh compton
+  sudo dnf install -y tilix NetworkManager-tui the_silver_searcher neovim tmux rofi arandr pavucontrol java-1.8.0-openjdk-devel feh compton xclip
   sudo dnf copr enable -y gregw/i3desktop
   sudo dnf install -y i3-gaps i3status i3lock i3blocks acpi
   sudo alternatives --set java java-1.8.0-openjdk.x86_64
   sudo dnf remove -y PackageKit-command-not-found
-  sudo dnf copr enable vishalvvr/fontawesome-fonts # Normal repo out of date
-  sudo dnf install fontawesome-free-fonts
+  sudo dnf copr enable -y vishalvvr/fontawesome-fonts # Normal repo out of date
+  sudo dnf install -y fontawesome-free-fonts
   pip3 install --user --upgrade pynvim
 fi
