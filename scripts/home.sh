34db94302e8cdc8ea63ca43e19cb2c6cc4032b0c
Kraig McKernan <kraigmckernan@gmail.com>
2020-04-24 09:42:07 -0600

    Polybar

--- scripts/home.sh
+++ scripts/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output DP-1-1 --off --output DP-1-2 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1-3 --off
+xrandr --output eDP-1 --off --output DP-2 --primary --mode 3840x1600 --pos 0x0 --rotate normal
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,8 @@
 if [[ -f "/etc/fedora-release" ]];
 then
-  sudo dnf install -y tilix NetworkManager-tui the_silver_searcher neovim tmux rofi arandr pavucontrol java-1.8.0-openjdk-devel feh compton xclip
+  sudo dnf install -y tilix NetworkManager-tui the_silver_searcher neovim tmux rofi arandr pavucontrol java-1.8.0-openjdk-devel feh compton xclip network-manager-applet polybar
   sudo dnf copr enable -y gregw/i3desktop
-  sudo dnf install -y i3-gaps i3status i3lock i3blocks acpi
+  sudo dnf install -y i3-gaps i3status i3lock acpi
   sudo alternatives --set java java-1.8.0-openjdk.x86_64
   sudo dnf remove -y PackageKit-command-not-found
   sudo dnf copr enable -y vishalvvr/fontawesome-fonts # Normal repo out of date
