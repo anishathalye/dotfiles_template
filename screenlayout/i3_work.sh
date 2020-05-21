bc3bb400921a2ae9948d48f9555b976ff39c0a73
lbesnard <laurent.besnard@utas.edu.au>
2020-05-21 18:02:33 +1000

    gitgnore + arandr

---
+++ screenlayout/i3_work.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 0x120 --rotate normal --output DP-2 --off
