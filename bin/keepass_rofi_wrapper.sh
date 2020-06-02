4643f340f52dffa9342c3fe4bf0734d119c61920
lbesnard <laurent.besnard@utas.edu.au>
2020-06-03 09:23:02 +1000

    wip

--- bin/keepass_rofi_wrapper.sh
+++ bin/keepass_rofi_wrapper.sh
@@ -3,4 +3,4 @@
 set -o allexport; . ~/.env_private; set +o allexport
 set -o allexport; . ~/.env; set +o allexport
 ROFI_KEEPASS_PATH=$GITHUB_DIR/rofi-keepassxc/rofi-keepassxc
-$ROFI_KEEPASS_PATH -d $KEEPASS_PATH
+$ROFI_KEEPASS_PATH -d $KEEPASS_PATH -t 6
