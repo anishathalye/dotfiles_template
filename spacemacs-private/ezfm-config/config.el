f509ec6d66ce19a1fae53955152d1db57f2a04f5
Ethan Miller <ethanzanemiller@gmail.com>
2020-08-18 11:19:06 -0700

    Setup org-journal

--- spacemacs-private/ezfm-config/config.el
+++ spacemacs-private/ezfm-config/config.el
@@ -19,3 +19,28 @@
 
 ;; ?
 (setq multi-term-program "/usr/local/bin/zsh")
+
+;; Org-mode customizations
+(setq org-todo-keywords
+      '((sequence "TODO" "STARTED" "|" "DONE")))
+
+;; Org-journal customization
+(setq org-journal-dir "~/Documents/org/journal/")
+(setq org-journal-file-type 'weekly)
+(setq org-journal-file-format "%Y-%m-%d")
+(setq org-journal-time-format "TODO ")
+(setq org-journal-enable-agenda-integration t)
+(setq org-journal-date-format "%A, %B %d %Y")
+
+(defun org-journal-date-format-func (time)
+  "Custom function to insert journal date header,
+and some custom text on a newly created journal file."
+  (when (= (buffer-size) 0)
+    (insert
+     (pcase org-journal-file-type
+       (`daily "#+TITLE: Daily Journal\n")
+       (`weekly (concat"#+TITLE: Weekly Journal " (format-time-string "(Wk #%V)" time) "\n"))
+       (`monthly "#+TITLE: Monthly Journal\n")
+       (`yearly "#+TITLE: Yearly Journal"))))
+  (concat org-journal-date-prefix (format-time-string "%A, %x" time)))
+(setq org-journal-date-format 'org-journal-date-format-func)
