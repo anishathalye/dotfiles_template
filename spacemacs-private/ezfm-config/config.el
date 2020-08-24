ae0ee7800cb8e4ff62d9db88c0b82b24a0a64502
Ethan Miller <ethanzanemiller@gmail.com>
2020-08-24 11:07:28 -0700

    Reoranizing org-journal customizations

--- spacemacs-private/ezfm-config/config.el
+++ spacemacs-private/ezfm-config/config.el
@@ -21,14 +21,8 @@
 (setq multi-term-program "/usr/local/bin/zsh")
 
 ;; Org-journal customization
-(setq org-agenda-file-regexp "\\`\\([^.].*\\.org\\|[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}\\(\\.gpg\\)?\\)\\'")
-(setq org-journal-dir "~/org/journal/")
-(setq org-journal-file-type 'weekly)
-(setq org-journal-file-format "%Y-%m-%d")
-(setq org-journal-time-prefix "** ")
-(setq org-journal-date-format "%A, %B %d %Y")
-;; (setq org-journal-enable-agenda-integration t)
 
+;; custom fns
 (defun org-journal-date-format-func (time)
   "Custom function to insert journal date header,
    and some custom text on a newly created journal file."
@@ -40,4 +34,16 @@
        (`monthly "#+TITLE: Monthly Journal\n\n")
        (`yearly "#+TITLE: Yearly Journal\n\n"))))
   (concat org-journal-date-prefix (format-time-string "%A, %x" time)))
+
+(setq org-agenda-file-regexp "\\`\\([^.].*\\.org\\|[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}\\(\\.gpg\\)?\\)\\'")
+(setq org-journal-dir "~/org/journal/")
+(setq org-journal-file-type 'weekly)
+(setq org-journal-file-format "%Y-%m-%d")
+;; (setq org-journal-time-format "")
+(setq org-journal-time-prefix "** ")
+(setq org-journal-date-format "%A, %B %d %Y")
+;; (setq org-journal-enable-agenda-integration t)
 (setq org-journal-date-format 'org-journal-date-format-func)
+
+
+
