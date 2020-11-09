eaa0c8ec7f1ed4054748468f56149a397ac96ff9
lbesnard <laurent.besnard@utas.edu.au>
2020-11-09 11:41:26 +1100

    wip

--- config/i3/scripts/display.sh
+++ config/i3/scripts/display.sh
@@ -44,9 +44,10 @@ fi
 
 # xrander will run and turn on the display you want, if you have an option for 3 displays, this will need some modifications
 case "$chosen" in
-    dual_home) xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 0x120 --rotate normal --output DP-2 --off; [[ -f ~/bin/mouse_setting.sh ]] && ~/bin/mouse_setting.sh;;
+#    dual_home) xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 0x120 --rotate normal --output DP-2 --off; [[ -f ~/bin/mouse_setting.sh ]] && ~/bin/mouse_setting.sh;;
+    dual_home) xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x112 --rotate normal --output DP-1-2 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --mode 1920x1200 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1-1 --off; [[ -f ~/bin/mouse_setting.sh ]] && ~/bin/mouse_setting.sh;;
     external) xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto --primary ;;
-    laptop) xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off ;; # xrandr --output $INTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT --off ;;
+    laptop) xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off ;; # xrandr --output $INTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT --off ;;
     clone) xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT ;;
     dual) xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --right-of $INTERNAL_OUTPUT --primary ;;
 esac
