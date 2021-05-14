eb5aa1ec682f10cddb3fccb1e945430fe995e3b8
lbesnard <laurent.besnard@utas.edu.au>
2021-05-14 13:54:56 +1000

    tmux better bar

--- initsys.sh
+++ initsys.sh
@@ -27,6 +27,15 @@ export INFOPATH="$HOMEBREW_PREFIX/share/info:$INFOPATH"
 # see https://github.com/Linuxbrew/homebrew-core/issues/955#issuecomment-250151297
 export HOMEBREW_ARCH=core2
 
+# install or upgrade brew packages
+function brew_install_or_upgrade {
+    if brew ls --versions "$1" >/dev/null; then
+        HOMEBREW_NO_AUTO_UPDATE=1; brew upgrade "$1"
+    else
+        HOMEBREW_NO_AUTO_UPDATE=1; brew install "$1"
+    fi
+}
+
 # install or upgrade packages
 brew_install_or_upgrade cmake
 brew_install_or_upgrade gcc
