38fe42ed612db31863553182d9d4c6cf9988cb94
lbesnard <laurent.besnard@utas.edu.au>
2021-08-25 12:53:55 +1000

    wip

---
+++ bin/beet_playlist_management.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# script automatically called by beet after import to clean playlists to be used
+# by different webapps
+
+IFS=$(echo -en "\n\b");
+
+# renaming the "relative_to" part of the playlist created in smartplaylist beet
+# pluging so this can be used in the airsonic docker container automatically
+for f in `find /export/music/playlists -type f`; do
+    sed -i "s#../beet_library#/music#g" "$f";
+done
+
+# for airsonic-advanced
+cp /export/music/playlists/* /export/music/playlists_airsonic_advanced/;
+for f in `find /export/music/playlists_airsonic_advanced -type f`; do
+    sed -i "s#^/music#/var/music#g" "$f";
+done
+
+# for navidrome
+cp /export/music/playlists/* /export/music/beet_library/playlists_navidrome/;
+for f in `find /export/music/beet_library/playlists_navidrome -type f`; do
+    sed -i "s#^/music#/music#g" "$f";
+done
