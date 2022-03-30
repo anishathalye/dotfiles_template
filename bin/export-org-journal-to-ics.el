f138e7b85fa3b7c0c2a7c0b06bd613f48b4ed31c
Ethan Miller <ethanzanemiller@gmail.com>
2022-03-29 21:01:40 -0700

    Update export-org-journal-to-ics.el script
    * Added some logging comments
    * Pointed load path for org-journal to cloned repo / submoulde in this project

--- bin/export-org-journal-to-ics.el
+++ bin/export-org-journal-to-ics.el
@@ -1,7 +1,13 @@
-;; no init file is loaded, so provide everything here:
-(add-to-list 'load-path "~/.emacs.d/elpa/org-journal-20210812.1749/")
+;; This script exports events located in my org journal files to an
+;; ICS file and then uploads to an S3 bucket. It relies on AWS-CLI
+;; and org-journal. 
+
+;; I installed org-journal as a subodule
+(add-to-list 'load-path "../org-journal/")
 
 (require 'org-journal)
+
+(defvar s3-destination "s3://ezmiller/calendar/org-events.ics")
 
 (with-eval-after-load 'org-journal
   (setq org-journal-dir "~/org/journal/")
@@ -9,13 +15,20 @@
   (setq org-journal-file-type 'weekly)
   (setq org-journal-file-format "%Y-%m-%d.org")
   (setq org-icalendar-store-UID t
-        org-icalendar-combined-name "OrgMode Tasks"
+        org-icalendar-combined-name "Org Events"
         org-icalendar-timezone "America/Los_Angeles"
         org-icalendar-date-time-format ";TZID=%Z:%Y%m%dT%H%M%S"
         org-icalendar-include-todo "all"
-        org-icalendar-combined-agenda-file "~/mytasks.ics")
+        org-icalendar-combined-agenda-file "./org-events.ics")
   (setq org-agenda-file-regexp "\\`\\([^.].*\\.org\\|[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}\\.org\\(\\.gpg\\)?\\)\\'"))
 
+(message "Starting...")
 (org-icalendar-combine-agenda-files)  ; export the ICS file
+(message "Finished exporting events to ICS file.")
+(message (format "Uploading ICS file to S3: %s" s3-destination) )
+(shell-command-to-string (format "aws s3 cp ./org-events.ics %s --acl public-read" s3-destination))
+(message "Cleaning up...")
+(shell-command-to-string "rm ./org-events.ics")
+(message "Done")
 (save-buffers-kill-emacs t)           ; save all modified files and exit
 
