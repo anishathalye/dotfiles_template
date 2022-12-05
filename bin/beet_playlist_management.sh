bae35e42ed39b7760c5e8f0a44889c0dba25a819
lbesnard <laurent.besnard@utas.edu.au>
2022-12-05 02:12:03 +0000

    wip

--- bin/beet_playlist_management.sh
+++ bin/beet_playlist_management.sh
@@ -6,19 +6,19 @@ IFS=$(echo -en "\n\b");
 
 # renaming the "relative_to" part of the playlist created in smartplaylist beet
 # pluging so this can be used in the airsonic docker container automatically
-for f in `find /export/music/playlists -type f`; do
+for f in `find /media/lbesnard/bfunk_10tb_3/music/playlists -type f`; do
     sed -i "s#../beet_library#/music#g" "$f";
 done
 
 # for airsonic-advanced
-cp /export/music/playlists/* /export/music/playlists_airsonic_advanced/;
-for f in `find /export/music/playlists_airsonic_advanced -type f`; do
+cp /media/lbesnard/bfunk_10tb_3/music/playlists/* /media/lbesnard/bfunk_10tb_3/music/playlists_airsonic_advanced/;
+for f in `find /media/lbesnard/bfunk_10tb_3/music/playlists_airsonic_advanced -type f`; do
     sed -i "s#^/music#/var/music#g" "$f";
 done
 
 # for navidrome
 # disabling this for now. I prefer to use the spotify pl. commenting
-#cp /export/music/playlists/* /export/music/beet_library/playlists_navidrome/;
-#for f in `find /export/music/beet_library/playlists_navidrome -type f`; do
+#cp /media/lbesnard/bfunk_10tb_3/music/playlists/* /media/lbesnard/bfunk_10tb_3/music/beet_library/playlists_navidrome/;
+#for f in `find /media/lbesnard/bfunk_10tb_3/music/beet_library/playlists_navidrome -type f`; do
     #sed -i "s#^/music#/music#g" "$f";
 #done
