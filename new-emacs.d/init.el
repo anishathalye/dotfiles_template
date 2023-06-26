91793fd3093b86982e6fbd97dba4ce14349285ba
Ethan Miller <ethanzanemiller@gmail.com>
2023-06-26 14:15:27 -0400

    Setup embark with wgrep

--- new-emacs.d/init.el
+++ new-emacs.d/init.el
@@ -404,7 +404,7 @@ same directory as the org-buffer and insert a link to this file."
     (setq column-number-mode t
           line-number-mode t)))
 
-;;; Vertico, etc.
+;;; Vertico, Consult, Embark, etc.
 
 (use-package vertico
   :init
@@ -443,6 +443,56 @@ same directory as the org-buffer and insert a link to this file."
   ;; switch between the annotators.
   ;; (setq marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
 )
+
+(use-package marginalia
+  :ensure t
+  :config
+  (marginalia-mode))
+
+(use-package embark
+  :ensure t
+
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc.  You may adjust the Eldoc
+  ;; strategy, if you want to see the documentation from multiple providers.
+  (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none))))
+
+
+
+  (general-define-key
+    "C-." 'embark-act             ; pick some action
+    "C-," 'embark-dwim            ; show context-based actions
+    "M-." 'embark-occur           ; occur-edit mode
+    "M-;" 'embark-export          ; export current view
+    "s"   'embark-isearch         ; live search
+    "/"   'embark-occur           ; occur-edit mode)
+  ))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t ; only need to install it, embark loads it after consult if found
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+(use-package wgrep)
 
 ;;; Version management
 (use-package magit
@@ -687,22 +737,22 @@ same directory as the org-buffer and insert a link to this file."
       (:name "✨ Finished ✨" :todo "DONE")))
   (org-agenda nil "a"))
 
-(use-package org-roam
-  :hook
-  (after-init . org-roam-mode)
-  :custom
-  (org-roam-directory "~/org/notes")
-  :init
-  (progn
-    ;; Define fn to fetch list of org-roam files (using with org-ql)
-    (defun org-roam-files ()
-      (org-ql-search-directories-files :recurse "~/org/notes"))
+;; (use-package 
+;;   :hook
+;;   (after-init . org-roam-mode)
+;;   :custom
+;;   (org-roam-directory "~/org/notes")
+;;   :init
+;;   (progn
+;;     ;; Define fn to fetch list of org-roam files (using with org-ql)
+;;     (defun org-roam-files ()
+;;       (org-ql-search-directories-files :recurse "~/org/notes"))
 
-    ;; (spacemacs/declare-prefix "ar" "org-roam")
-    (spc-key-definer
-      "arf" 'org-roam-node-find))
-  :config
-  (org-roam-setup))
+;;     ;; (spacemacs/declare-prefix "ar" "org-roam")
+;;     (spc-key-definer
+;;       "arf" 'org-roam-node-find))
+;;   :config
+;;   (org-roam-setup))
 
 (use-package org-ref
   :config
