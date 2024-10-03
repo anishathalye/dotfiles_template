ba2cc0b6f6ea94fced3b3cc9e39b32fb816749e8
Ethan Miller <ethanzanemiller@gmail.com>
2024-10-03 09:32:00 -0400

    Add step in install alacritty script to setup completions

--- install-alacritty.sh
+++ install-alacritty.sh
@@ -5,3 +5,13 @@ cd "$(dirname "$0")/alacritty"
 make app
 cp -r target/release/osx/Alacritty.app /Applications/
 sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+
+# Alacritty shell completions
+# See: https://github.com/alacritty/alacritty/blob/master/INSTALL.md#zsh
+
+# Create the directory for Alacritty shell completions if it doesn't already exist.
+if [ ! -d ${ZDOTDIR:-~}/.zsh_functions ]; then
+    mkdir ${ZDOTDIR:-~}/.zsh_functions
+    cp extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
+fi
+
