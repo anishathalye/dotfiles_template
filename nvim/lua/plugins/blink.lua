22f2f7284a7707cfeeaffaf289b500fb601b3bf4
lbesnard <laurent.besnard@utas.edu.au>
2025-01-22 16:08:04 +1100

    Fix: neovim complete

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -17,7 +17,9 @@ return {
     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = "default" },
+    keymap = {
+      preset = "super-tab",
+    },
 
     appearance = {
       -- Sets the fallback highlight groups to nvim-cmp's highlight groups
@@ -28,6 +30,7 @@ return {
       -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = "mono",
     },
+    signature = { enabled = true },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -4,5 +4,4 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   -- or if using mini.icons/mini.nvim
   -- dependencies = { "echasnovski/mini.icons" },
-  opts = {},
 }
